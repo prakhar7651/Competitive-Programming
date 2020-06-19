3c8559f9bc62b9f32f43ad27155d92304d4caeef
Prakhar Agarwal <56273982+PrAkHaR-BitsPilani@users.noreply.github.com>
2020-06-19 23:32:41 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -19,6 +19,18 @@ ll int Min(ll a,ll b){return a<b?a:b;}
 ll int Max(ll a,ll b){return a>b?a:b;}
 ll int Abs(ll a){ return a<0?-a:a;}
 ll int GIF(ll a,ll b){return a/b+(a%b!=0);}
+ll unsigned int bin_exp(ll int a,ll int b)
+{
+    ll unsigned result = 1;
+    while(b)
+    {
+        if(b&1)
+        result*=a;
+        a*=a;
+        b>>=1;
+    }
+    return result;
+}
 mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
 int rng(int lim) {
 	uniform_int_distribution<int> uid(0,lim-1);
