f9c5bd1fd3ec39e145780e5a9e32908e9a02c9b6
Prakhar Agarwal <56273982+PrAkHaR-BitsPilani@users.noreply.github.com>
2020-06-19 10:50:40 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define eb emplace_back
+#define mp make_pair
+#define ll long long
+#define ull unsigned ll
+#define rep(i,n) for(int i = 0;i<n;i++)
+#define REP(i,k,n) for(int i=k;i<n;i++)
+#define sz(x) ((int)x.size())
+#define IOS ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define b_search binary_search
+#define largest *max_element
+#define smallest *min_element
+using namespace std;
+ll int Min(ll a,ll b){return a<b?a:b;}
+ll int Max(ll a,ll b){return a>b?a:b;}
+ll int Abs(ll a){ return a<0?-a:a;}
+ll int GIF(ll a,ll b){return a/b+(a%b!=0);}
+void solve()
+{
+
+}
+int32_t main()
+{
+    IOS;
+    //freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+    int tc = 1;
+    cin>>tc;
+    while(tc--)
+    {
+        solve();
+    }
+    return 0;
+}
