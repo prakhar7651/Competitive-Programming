16a66b685f0ece8da7d2002fd148f23a864fcd9e
PrAkHaR-BitsPilani <56273982+PrAkHaR-BitsPilani@users.noreply.github.com>
2020-07-21 17:31:29 +0530

    added LCP array

--- Coding Library/Algorithms/Strings/Suffix_Array.cpp
+++ Coding Library/Algorithms/Strings/Suffix_Array.cpp
@@ -90,3 +90,25 @@ void BuildSuffixArray(string* s,int* order)//(S SHOULD HAVE $ IN THE END)order s
         L = L*2;
     }
 }
+void buildLCParray(string* s,int* order,int* lcp) // lcp[i] = LCP(order[i],order[i+1])
+{
+    int rank[s->length()];
+    for (int i = 0; i < s->length(); i++)
+    {
+        rank[*(order + i)] = i;
+    }
+    int k=0;
+    for (int i = 0; i < s->length(); i++)
+    {
+        if(rank[i] == s->length()-1)
+        {
+            k=0;
+            continue;
+        }
+        int j = *(order + rank[i] + 1);
+        while(i + k < s->length() && j + k < s->length() && s->at(i+k) == s->at(j+k))
+            k++;
+        lcp[rank[i]] = k;
+        k = max(0,k-1);
+    }    
+}
