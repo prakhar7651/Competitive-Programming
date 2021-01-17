2939fe30e64415959546e30e85eb156a185c22fd
PrAkHaR-BitsPilani <56273982+PrAkHaR-BitsPilani@users.noreply.github.com>
2021-01-17 13:25:34 +0530

    naming

--- Coding Library/Data Structures/Trees/Segment Trees/min_query.cpp
+++ Coding Library/Data Structures/Trees/Segment Trees/min_query.cpp
@@ -13,28 +13,28 @@ struct SegTree{
     size <<= 1;
     arr.assign(size,0);   
   }
-  void fill(int* a,int x,int l,int r)
+  void fill(int* a,int index,int l,int r)
   {
     if(l==(r-1))
     {
-      arr[x] = *(a+l);
+      arr[index] = *(a+l);
       return;
     }
     int m = l + (r-l)/2;
-    fill(a,2*x+1,l,m);
-    fill(a,2*x+2,m,r);
-    arr[x] =  min(arr[2*x+1],arr[2*x+2]);
+    fill(a,2*index+1,l,m);
+    fill(a,2*index+2,m,r);
+    arr[index] =  min(arr[2*index+1],arr[2*index+2]);
   }
-  int getMin(int l,int r,int low,int high,int x)
+  int getMin(int l,int r,int low,int high,int index)
   {
     if(low<=l && (r-1) <= high)
-      return arr[x];
+      return arr[index];
     if(l > high || (r-1) < low)
       return INT_MAX;
     int m = l + (r-l)/2;
-    return min(getMin(l,m,low,high,2*x+1),getMin(m,r,low,high,2*x+2));
+    return min(getMin(l,m,low,high,2*index+1),getMin(m,r,low,high,2*index+2));
   }
-  void update(int i,int new_val,int l,int r,int x)
+  void update(int i,int new_val,int l,int r,int index)
   {
     if ( i < l || i >= r)
       return;
@@ -42,14 +42,14 @@ struct SegTree{
       {
         if(l == (r-1))
           {
-            arr[x] = new_val;
+            arr[index] = new_val;
             return;
           }
         int m = l + ((r-l)>>1);
         if(i < m)
-          update(i,new_val,l,m,2*x+1);
-        else update(i,new_val,m,r,2*x+2);
-        arr[x]  = min(arr[2*x+1],arr[2*x+2]);
+          update(i,new_val,l,m,2*index+1);
+        else update(i,new_val,m,r,2*index+2);
+        arr[index]  = min(arr[2*index+1],arr[2*index+2]);
       }
   }
 };
