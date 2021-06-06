36ae44932c8a118e6188922e563231da7d8d42d4
PrAkHaR-BitsPilani <56273982+PrAkHaR-BitsPilani@users.noreply.github.com>
2021-06-06 17:44:50 +0530

    updated

--- template.cpp
+++ template.cpp
@@ -44,7 +44,7 @@ ll unsigned bin_exp(ll int a,ll int b){ll unsigned result = 1;while(b){if(b&1)re
 mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
 int rng(int lim) {uniform_int_distribution<int> uid(0,lim-1);return uid(rang);}
 const int mod = 1'000'000'007; //1e9+7
-const int N = 1e5+5;
+const int N = 2e5+5;
 #define int ll
 #ifdef int
 #define INF LLONG_MAX
